fadc4a35591903787d0d29dbb8d3d64a27cfc3ff
tbazarov <159284610+tbazarov@users.noreply.github.com>
2025-04-17 13:13:41 +0700

    Update bit_arr.cpp

--- bit_arr.cpp
+++ bit_arr.cpp
@@ -3,79 +3,80 @@
 #include <algorithm>
 #include <sstream>
 
-// Базовый конструктор
-BitArray::BitArray() : value(nullptr), size_bits(0), capacity(0) {} 
+BitArray::BitArray() : value(nullptr), size_bits(0), capacity(0) {}
 
-// Деструктор
 BitArray::~BitArray() {
     delete[] value;
 }
 
-// Конструктор с указанием количества бит и начального значения
 BitArray::BitArray(int size_bits, unsigned long value) {
-    if (size_bits < 0) throw std::invalid_argument("Числа не должны быть отрицательными");
+    if (size_bits < 0) throw std::invalid_argument("Кол-во битов не может быть отрицательным");
     allocate_memory(size_bits);
-    for (int i = 0; i < size_bits; ++i) {
+    for (int i = 0; i < size_bits && i < static_cast<int>(sizeof(unsigned long) * 8); ++i) {
         set(i, (value >> i) & 1);
     }
 }
 
-// Конструктор копирования
-BitArray::BitArray(const BitArray& b) {
-    copy_from(b);
+BitArray::BitArray(const BitArray& b) : size_bits(b.size_bits), capacity(b.capacity) {
+    if (capacity > 0) {
+        value = new char[capacity];
+        memcpy(value, b.value, capacity);
+    } else {
+        value = nullptr;
+    }
 }
 
-// обмен двух массивов
 void BitArray::swap(BitArray& b) {
     std::swap(value, b.value);
     std::swap(size_bits, b.size_bits);
     std::swap(capacity, b.capacity);
 }
 
-// Оператор присваивания
 BitArray& BitArray::operator=(const BitArray& b) {
     if (this != &b) {
-        delete[] value;
-        copy_from(b);
+        BitArray temp(b);
+        swap(temp);
     }
     return *this;
 }
 
-// Изменение размера массива
-void BitArray::resize(int size_bits, bool value) {
-    if (size_bits < 0) throw std::invalid_argument("Число битов не может быть отрицательным");
-    if (size_bits == this->size_bits) return;
-
-    BitArray temp(*this);
-    allocate_memory(size_bits);
-
-    int min_bits = std::min(this->size_bits, size_bits);
-    for (int i = 0; i < min_bits; ++i) {
-        set(i, temp[i]);
+void BitArray::resize(int new_size, bool val) {
+    if (new_size < 0) throw std::invalid_argument("Размер не может быть отрицательным");
+    
+    if (new_size <= size_bits) {
+        size_bits = new_size;
+        return;
     }
-
-    for (int i = min_bits; i < size_bits; ++i) {
-        set(i, value);
+    
+    int needed_capacity = ((new_size + 7) / 8) * 2;
+    if (needed_capacity > capacity) {
+        char* new_value = new char[needed_capacity];
+        memset(new_value + capacity, 0, needed_capacity - capacity);
+        if (value) {
+            memcpy(new_value, value, capacity);
+            delete[] value;
+        }
+        value = new_value;
+        capacity = needed_capacity;
     }
+    
+    for (int i = size_bits; i < new_size; ++i) {
+        set(i, val);
+    }
+    size_bits = new_size;
 }
 
-// Очистка массива
 void BitArray::clear() {
-    delete[] value;
-    value = nullptr;
     size_bits = 0;
-    capacity = 0;
 }
 
-// Добавление нового бита
 void BitArray::push_back(bool bit) {
     resize(size_bits + 1, bit);
 }
 
-// Битовые операции
 BitArray& BitArray::operator&=(const BitArray& b) {
     check_size_compatibility(b);
-    for (int i = 0; i < capacity; ++i) {
+    for (int i = 0; i < (size_bits + 7) / 8; ++i) {
         value[i] &= b.value[i];
     }
     return *this;
@@ -83,7 +84,7 @@ BitArray& BitArray::operator&=(const BitArray& b) {
 
 BitArray& BitArray::operator|=(const BitArray& b) {
     check_size_compatibility(b);
-    for (int i = 0; i < capacity; ++i) {
+    for (int i = 0; i < (size_bits + 7) / 8; ++i) {
         value[i] |= b.value[i];
     }
     return *this;
@@ -91,32 +92,14 @@ BitArray& BitArray::operator|=(const BitArray& b) {
 
 BitArray& BitArray::operator^=(const BitArray& b) {
     check_size_compatibility(b);
-    for (int i = 0; i < capacity; ++i) {
+    for (int i = 0; i < (size_bits + 7) / 8; ++i) {
         value[i] ^= b.value[i];
     }
     return *this;
 }
 
-
-// Сдвиги
 BitArray& BitArray::operator<<=(int n) {
-    if (n < 0) throw std::invalid_argument("Неправильный сдвиг");
-    if (n >= size_bits) {
-        reset();
-        return *this;
-    }
-
-    for (int i = size_bits - 1; i >= n; --i) {
-        set(i, operator[](i - n));
-    }
-    for (int i = 0; i < n; ++i) {
-        reset(i);
-    }
-    return *this;
-}
-
-BitArray& BitArray::operator>>=(int n) {
-    if (n < 0) throw std::invalid_argument("Неправильный сдвиг");
+    if (n < 0) throw std::invalid_argument("Количество сдвигов не может быть отрицательным");
     if (n >= size_bits) {
         reset();
         return *this;
@@ -126,6 +109,22 @@ BitArray& BitArray::operator>>=(int n) {
         set(i, operator[](i + n));
     }
     for (int i = size_bits - n; i < size_bits; ++i) {
+        reset(i);
+    }
+    return *this;
+}
+
+BitArray& BitArray::operator>>=(int n) {
+    if (n < 0) throw std::invalid_argument("Количество сдвигов не может быть отрицательным");
+    if (n >= size_bits) {
+        reset();
+        return *this;
+    }
+
+    for (int i = size_bits - 1; i >= n; --i) {
+        set(i, operator[](i - n));
+    }
+    for (int i = 0; i < n; ++i) {
         reset(i);
     }
     return *this;
@@ -143,9 +142,8 @@ BitArray BitArray::operator>>(int n) const {
     return result;
 }
 
-// Установка битов
 BitArray& BitArray::set(int n, bool val) {
-    if (n < 0 || n >= size_bits) throw std::out_of_range("Выход за пределы");
+    if (n < 0 || n >= size_bits) throw std::out_of_range("Выход за границу");
     int char_index = n / 8;
     int bit_index = n % 8;
     if (val) {
@@ -157,7 +155,7 @@ BitArray& BitArray::set(int n, bool val) {
 }
 
 BitArray& BitArray::set() {
-    memset(value, 0xFF, capacity);
+    memset(value, 0xFF, (size_bits + 7) / 8);
     return *this;
 }
 
@@ -166,25 +164,24 @@ BitArray& BitArray::reset(int n) {
 }
 
 BitArray& BitArray::reset() {
-    memset(value, 0, capacity);
+    memset(value, 0, (size_bits + 7) / 8);
     return *this;
 }
 
-// Проверки
-bool BitArray::any() const{
-    for (int i = 0; i < capacity; ++i) {
+bool BitArray::any() const {
+    for (int i = 0; i < (size_bits + 7) / 8; ++i) {
         if (value[i]) return true;
     }
     return false;
 }
 
-bool BitArray::none() const{
+bool BitArray::none() const {
     return !any();
 }
 
-BitArray BitArray::operator~() const{
+BitArray BitArray::operator~() const {
     BitArray result(*this);
-    for (int i = 0; i < capacity; ++i) {
+    for (int i = 0; i < (size_bits + 7) / 8; ++i) {
         result.value[i] = ~value[i];
     }
     return result;
@@ -193,75 +190,47 @@ BitArray BitArray::operator~() const{
 int BitArray::count() const {
     int count = 0;
     for (int i = 0; i < size_bits; ++i) {
-        if (operator[](i)) ++count;
+        count += operator[](i);
     }
     return count;
 }
 
 bool BitArray::operator[](int i) const {
-    if (i < 0 || i >= size_bits) throw std::out_of_range("Выход за пределы");
+    if (i < 0 || i >= size_bits) throw std::out_of_range("Выход за границу");
     int char_index = i / 8;
     int bit_index = i % 8;
     return (value[char_index] >> bit_index) & 1;
 }
 
-// Доступ к битам
-// bool BitArray::operator[](int i) {
-//     if (i < 0 || i >= size_bits) throw std::out_of_range("Выход за пределы");
-//     int char_index = i / 8;
-//     int bit_index = i % 8;
-//     return (value[char_index] >> bit_index) & 1;
-// }
-
-// int BitArray::size_bits() {
-//     return size_bits;
-// }
-
-int BitArray::size() const{
+int BitArray::size() const {
     return size_bits;
 }
 
-bool BitArray::empty() const{
+bool BitArray::empty() const {
     return size_bits == 0;
 }
 
-std::string BitArray::to_string() {
-    std::ostringstream oss;
+std::string BitArray::to_string() const {
+    std::string result(size_bits, '0');
     for (int i = 0; i < size_bits; ++i) {
-        oss << (operator[](i) ? '1' : '0');
+        result[i] = '0' + operator[](i);
     }
-    return oss.str();
+    return result;
 }
 
-// Приватные методы
 void BitArray::allocate_memory(int size_bits) {
     this->size_bits = size_bits;
-    capacity = (size_bits + 8 - 1) / 8;
+    capacity = ((size_bits + 7) / 8) * 2;
     value = new char[capacity];
     memset(value, 0, capacity);
 }
 
-void BitArray::copy_from(const BitArray& b) {
-    size_bits = b.size_bits;
-    capacity = b.capacity;
-
-    if (value) 
-    {
-        delete[] value; // очистка памяти, если она до этого была занята
-    }
-
-    value = new char[capacity];
-    memcpy(value, b.value, capacity);
-    
-}
-
 void BitArray::check_size_compatibility(const BitArray& b) const {
     if (size_bits != b.size_bits) {
-        throw std::invalid_argument("Для побитовых операций массивы BitArrays должны иметь одинаковый размер.");
+        throw std::invalid_argument("Массивы должны иметь одинаковый размер");
     }
 }
 
-// операторы сравнения
 bool operator==(const BitArray& a, const BitArray& b) {
     if (a.size() != b.size()) return false;
     for (int i = 0; i < a.size(); ++i) {
